7dfaa8954697c59d82042236ff9e43d0e0e23ff2
Rafael Clemente <rafaeljclemente@dcc.ufrj.br>
2023-10-26 16:00:34 -0300

    Tabs substituídos por espaços

--- trabalho-2/mandlebrot.c
+++ trabalho-2/mandlebrot.c
@@ -16,86 +16,86 @@ typedef struct complextype {
 
 
 void main() {
-	Window		win;                            /* initialization for a window */
-	unsigned
-	int         width, height,                  /* window size */
+    Window		win;                            /* initialization for a window */
+    unsigned
+    int         width, height,                  /* window size */
                 x, y,                           /* window position */
-                border_width,                   /*border width in pixels */
+                border_width,                   /* border width in pixels */
                 display_width, display_height,  /* size of screen */
                 screen;                         /* which screen */
 
-	char        *window_name = "Mandelbrot Set", *display_name = NULL;
-	GC          gc;
-	unsigned
-	long		valuemask = 0;
-	XGCValues	values;
-	Display		*display;
-	XSizeHints	size_hints;
-	Pixmap		bitmap;
-	XPoint		points[800];
-	FILE		*fp, *fopen();
-	char		str[100];
-	
-	XSetWindowAttributes attr[1];
+    char        *window_name = "Mandelbrot Set", *display_name = NULL;
+    GC          gc;
+    unsigned
+    long        valuemask = 0;
+    XGCValues	values;
+    Display		*display;
+    XSizeHints	size_hints;
+    Pixmap		bitmap;
+    XPoint		points[800];
+    FILE        *fp, *fopen();
+    char        str[100];
+    
+    XSetWindowAttributes attr[1];
 
     /* Mandlebrot variables */
-    int     i, j, k;
+    int		i, j, k;
     Compl	z, c;
     float	lengthsq, temp;
        
-	/* connect to Xserver */
-	if ((display = XOpenDisplay(display_name)) == NULL) {
-	    fprintf(stderr, "drawon: cannot connect to X server %s\n",
-			    XDisplayName(display_name));
-	    exit(-1);
-	}
-	
-	/* get screen size */
-	screen = DefaultScreen(display);
-	display_width = DisplayWidth(display, screen);
-	display_height = DisplayHeight(display, screen);
+    /* connect to Xserver */
+    if ((display = XOpenDisplay(display_name)) == NULL) {
+        fprintf(stderr, "drawon: cannot connect to X server %s\n",
+                XDisplayName(display_name));
+        exit(-1);
+    }
+    
+    /* get screen size */
+    screen = DefaultScreen(display);
+    display_width = DisplayWidth(display, screen);
+    display_height = DisplayHeight(display, screen);
 
-	/* set window size */
-	width = X_RESN;
-	height = Y_RESN;
+    /* set window size */
+    width = X_RESN;
+    height = Y_RESN;
 
-	/* set window position */
-	x = 0;
-	y = 0;
+    /* set window position */
+    x = 0;
+    y = 0;
 
     /* create opaque window */
-	border_width = 4;
-	win = XCreateSimpleWindow(display, RootWindow(display, screen),
-				x, y, width, height, border_width, 
-				BlackPixel(display, screen), WhitePixel(display, screen));
+    border_width = 4;
+    win = XCreateSimpleWindow(display, RootWindow(display, screen),
+                x, y, width, height, border_width, 
+                BlackPixel(display, screen), WhitePixel(display, screen));
 
-	size_hints.flags = USPosition|USSize;
-	size_hints.x = x;
-	size_hints.y = y;
-	size_hints.width = width;
-	size_hints.height = height;
-	size_hints.min_width = 300;
-	size_hints.min_height = 300;
-	
-	XSetNormalHints(display, win, &size_hints);
-	XStoreName(display, win, window_name);
+    size_hints.flags = USPosition|USSize;
+    size_hints.x = x;
+    size_hints.y = y;
+    size_hints.width = width;
+    size_hints.height = height;
+    size_hints.min_width = 300;
+    size_hints.min_height = 300;
+    
+    XSetNormalHints(display, win, &size_hints);
+    XStoreName(display, win, window_name);
 
     /* create graphics context */
-	gc = XCreateGC(display, win, valuemask, &values);
+    gc = XCreateGC(display, win, valuemask, &values);
 
-	XSetBackground(display, gc, WhitePixel(display, screen));
-	XSetForeground(display, gc, BlackPixel(display, screen));
-	XSetLineAttributes(display, gc, 1, LineSolid, CapRound, JoinRound);
+    XSetBackground(display, gc, WhitePixel(display, screen));
+    XSetForeground(display, gc, BlackPixel(display, screen));
+    XSetLineAttributes(display, gc, 1, LineSolid, CapRound, JoinRound);
 
-	attr[0].backing_store = Always;
-	attr[0].backing_planes = 1;
-	attr[0].backing_pixel = BlackPixel(display, screen);
+    attr[0].backing_store = Always;
+    attr[0].backing_planes = 1;
+    attr[0].backing_pixel = BlackPixel(display, screen);
 
-	XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
+    XChangeWindowAttributes(display, win, CWBackingStore | CWBackingPlanes | CWBackingPixel, attr);
 
-	XMapWindow(display, win);
-	XSync(display, 0);
-      	 
+    XMapWindow(display, win);
+    XSync(display, 0);
+           
     /* Calculate and draw points */
     for (i = 0; i < X_RESN; i++) 
         for (j = 0; j < Y_RESN; j++) {
@@ -115,9 +115,9 @@ void main() {
             if (k == 100) 
                 XDrawPoint(display, win, gc, j, i);
         }
-	 
-	XFlush(display);
-	sleep(30);
+     
+    XFlush(display);
+    sleep(30);
 
-	/* Program Finished */
+    /* Program Finished */
 }
